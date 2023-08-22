332cf77ad821747e6a909cabddf9beb9f249a791
Apu Dutta <adutta158@gmail.com>
2023-08-21 21:26:38 -0700

    Initial Commit
    Created a blank project in UE5 (v5.2.1) using the third person template, C++ project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bounsea
+
+Developed with Unreal Engine 5
---
+++ Source/Bounsea/BounseaGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BounseaGameMode.h"
+#include "BounseaCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ABounseaGameMode::ABounseaGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
