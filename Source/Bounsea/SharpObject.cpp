5c4af79e0242885e8296022a05514f4bde0cfda1
Remmiemmyjr <emily@isimar.com>
2023-09-02 19:33:27 -0700

    Added sharp object class and created sharp object bp

---
+++ Source/Bounsea/SharpObject.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SharpObject.h"
+
+// Sets default values
+ASharpObject::ASharpObject()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASharpObject::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASharpObject::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
