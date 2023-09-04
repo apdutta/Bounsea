327622dc4b0d5f588f815f27d5280d65726fc3ea
Apu Dutta <adutta158@gmail.com>
2023-09-04 09:48:36 -0700

    Fix compile error

--- Source/Bounsea/BounseaCharacter.cpp
+++ Source/Bounsea/BounseaCharacter.cpp
@@ -50,7 +50,6 @@ ABounseaCharacter::ABounseaCharacter()
 
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
-	isInAir = false;
 }
 
 void ABounseaCharacter::BeginPlay()
