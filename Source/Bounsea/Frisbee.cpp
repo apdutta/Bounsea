a50bddd5f1b478b5de9074c631eb303d65784ae1
iceblue64 <92569146+iceblue64@users.noreply.github.com>
2023-09-03 22:11:08 -0700

    Small Frisbee Update
    Added rotation code, and instantiated an instance in the level. Unsure why it doesn't work — I ensured that the rotation wasn't locked on the BP.

--- Source/Bounsea/Frisbee.cpp
+++ Source/Bounsea/Frisbee.cpp
@@ -23,16 +23,16 @@ void AFrisbee::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
 
-    // Get physics component and add force vector to propel it on the x-axis
+    // Get physics component and propel it on the x-axis
     UPrimitiveComponent* physics = FindComponentByClass<UPrimitiveComponent>();
     if (physics)
     {
-        FVector force = FVector(1, 0, 0) * speedScale; // change magic number
+        FVector force = FVector(1, 0, 0) * speedScale;
         physics->SetPhysicsLinearVelocity(force);
-    }
-    else
-        UE_LOG(LogTemp, Error, TEXT("Physics component not found on frisbee"));
 
-    //FRotator rotate = FRotator(1, 0, 0);
-    //AddActorLocalRotation(rotate);
+    }
+
+    // Rotate the frisbee
+    FRotator rotate = FRotator(0, 0, 5);
+    AddActorLocalRotation(rotate);
 }
